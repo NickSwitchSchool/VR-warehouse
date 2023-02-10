186ae2c748a7c0aa216105b2ded76b90f63facfd
NickSwitchSchool <97087180@st.deltion.nl>
2023-02-10 10:58:50 +0100

    mogelijkheid om te testen zonder VR bril

--- VR warehouse/Assets/Scripts/PlayerScript.cs
+++ VR warehouse/Assets/Scripts/PlayerScript.cs
@@ -8,8 +8,16 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] float speed;
     Vector3 movement;
     Rigidbody playerRB;
+    [SerializeField] GameObject rightHand;
+    [SerializeField] GameObject beam;
+    RaycastHit tpCheck;
     [Space(20)]
     [Header("Camera")]
+    [SerializeField] bool testWithoutVR;
+    Vector3 rotation;
+    Vector3 camrotation;
+    float mouseVertical;
+    [Space(10)]
     [SerializeField] float sensitivity;
     [SerializeField] float maxXRotation;
     [SerializeField] float quickRotateInterval;
@@ -25,17 +33,55 @@ public class PlayerScript : MonoBehaviour
     private void Update()
     {
         //camera
-        Vector2 rotateCam = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
-        if (rotateCam.x >= .5f)
+        Vector2 rightJoystick = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick);
+        if (rightJoystick.x >= .5f)
         {
             QuickRotate(quickRotateDegrees);
         }
-        else if (rotateCam.x <= -.5f)
+        else if (rightJoystick.x <= -.5f)
         {
             QuickRotate(-quickRotateDegrees);
         }
         Quaternion headsetRotation = cameraRig.centerEyeAnchor.rotation;
         cam.transform.rotation = headsetRotation;
+
+        //camera without VR
+        if (testWithoutVR)
+        {
+            rotation.y += Input.GetAxis("Mouse X") * sensitivity;
+            transform.eulerAngles = rotation;
+            camrotation.y = rotation.y;
+            mouseVertical = Input.GetAxis("Mouse Y");
+            if (camrotation.x < maxXRotation && camrotation.x > -maxXRotation)
+            {
+                camrotation.x -= mouseVertical * sensitivity;
+            }
+            else if (camrotation.x >= maxXRotation && mouseVertical > 0)
+            {
+                camrotation.x -= mouseVertical * sensitivity;
+            }
+            else if (camrotation.x <= -maxXRotation && mouseVertical < 0)
+            {
+                camrotation.x -= mouseVertical * sensitivity;
+            }
+            cam.transform.eulerAngles = camrotation;
+        }
+
+        //teleport
+        rightHand.transform.localPosition = cameraRig.rightHandAnchor.position + new Vector3(0, .9f, 0) + transform.forward * 0.2f;
+        rightHand.transform.localRotation = cameraRig.rightHandAnchor.rotation;
+        if (OVRInput.Get(OVRInput.Button.Two))
+        {
+            beam.SetActive(true);
+        }
+        else if (OVRInput.GetUp(OVRInput.Button.Two))
+        {
+            beam.SetActive(false);
+            if (Physics.Raycast(rightHand.transform.position, rightHand.transform.forward, out tpCheck, 100))
+            {
+                transform.position = tpCheck.point + new Vector3(0, .3f, 0);
+            }
+        }
     }
 
     private void FixedUpdate()
@@ -60,6 +106,7 @@ public class PlayerScript : MonoBehaviour
         {
             intervalTimer = 0;
             cameraRig.gameObject.transform.Rotate(0, degrees, 0);
+            rightHand.transform.Rotate(0, -degrees, 0);
         }
     }
 }
