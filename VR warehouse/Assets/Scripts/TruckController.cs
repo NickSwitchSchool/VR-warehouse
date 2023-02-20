ad6244d67f2075ccb12f54c82c198d14d989c5a2
Xander Veninga <39034729+EliteForce1@users.noreply.github.com>
2023-02-20 15:11:38 +0100

    trucks completed

--- VR warehouse/Assets/Scripts/TruckController.cs
+++ VR warehouse/Assets/Scripts/TruckController.cs
@@ -11,6 +11,7 @@ public class TruckController : MonoBehaviour
 {
     public InventoryManager inventoryManager;
     public ExportManager exportManager;
+    public List<Product> exportNeeds;
     public List<Product> truckInventory;
     public enum PortType {Import, Export}
     public PortType task;
@@ -32,11 +33,12 @@ public class TruckController : MonoBehaviour
         inventoryManager = exportManager.inventoryManager;
         if(task == PortType.Export)
         {
-
+            RandomOrder(task);
         }
         if(task == PortType.Import)
         {
-            RandomOrder();
+
+            RandomOrder(task);
         }
         startTime = Time.time;
         travelTime = Vector3.Distance(startPos.position, endPos.position);
@@ -51,47 +53,87 @@ public class TruckController : MonoBehaviour
 
             gameObject.transform.position = Vector3.Lerp(startPos.position, endPos.position, fracComplete);
         }
-        if(gameObject.transform.position == endPos.position)
+        if(gameObject.transform.position == endPos.position && !onSpot)
         {
+            onSpot = true;
             doorState = true;
             ToggleDoors(task);
+            
         }
     }
-    public void RandomOrder()
+    public void RandomOrder(PortType type)
     {
         for (int i = 0; i < 7; i++)
         {
             Product virtualProduct = inventoryManager.productList.ElementAt(Random.Range(0, inventoryManager.productList.Count));
             Product newProduct = new Product(virtualProduct.productAmount, virtualProduct.productObject);
-            if (truckInventory.Count > 0)
+            switch (type)
             {
-                bool addProduct = true;
-                int productInd = 0;
-                for (int a = 0; a < truckInventory.Count; a++)
-                {
-                    if (truckInventory[a].productObject == newProduct.productObject)
+                case PortType.Import:
+                    if (truckInventory.Count > 0)
                     {
-                        truckInventory[a].productAmount++;
-                        addProduct = false;
-                        break;
+                        bool addProduct = true;
+                        int productInd = 0;
+                        for (int a = 0; a < truckInventory.Count; a++)
+                        {
+                            if (truckInventory[a].productObject == newProduct.productObject)
+                            {
+                                truckInventory[a].productAmount++;
+                                addProduct = false;
+                                break;
+                            }
+                            else
+                            {
+                                productInd++;
+                            }
+                        }
+                        if (addProduct)
+                        {
+                            truckInventory.Add(newProduct);
+                            truckInventory[productInd].productAmount++;
+                        }
                     }
                     else
                     {
-                        productInd++;
+                        truckInventory.Add(newProduct);
+                        truckInventory[0].productAmount++;
                     }
-                }
-                if(addProduct)
-                {
-                    truckInventory.Add(newProduct);
-                    truckInventory[productInd].productAmount++;
-                }
+
+                    break;
+
+                case PortType.Export:
+                    if (exportNeeds.Count > 0)
+                    {
+                        bool addProduct = true;
+                        int productInd = 0;
+                        for (int a = 0; a < exportNeeds.Count; a++)
+                        {
+                            if (exportNeeds[a].productObject == newProduct.productObject)
+                            {
+                                exportNeeds[a].productAmount++;
+                                addProduct = false;
+                                break;
+                            }
+                            else
+                            {
+                                productInd++;
+                            }
+                        }
+                        if (addProduct)
+                        {
+                            exportNeeds.Add(newProduct);
+                            exportNeeds[productInd].productAmount++;
+                        }
+                    }
+                    else
+                    {
+                        exportNeeds.Add(newProduct);
+                        exportNeeds[0].productAmount++;
+                    }
+
+                    break;
             }
-            else
-            {
-                truckInventory.Add(newProduct);
-                truckInventory[0].productAmount++;
-            }
-        }
+        }    
 
     }
 
@@ -110,12 +152,44 @@ public class TruckController : MonoBehaviour
     }
     public void EmptyTruck()
     {
-
+        if (truckInventory.Count <= 0 && onSpot)
+        {
+            StartCoroutine(CloseDoor());
+        }
     }
 
     public void FillTruck()
     {
-        
+        if (exportNeeds.Count <= 0 && onSpot)
+        {
+            StartCoroutine(CloseDoor());
+        }
     }
-    
+
+    IEnumerator CloseDoor()
+    {
+        doorState = false;
+        ToggleDoors(this.GetComponent<TruckController>().task);
+        yield return new WaitForSeconds(5);
+        if(onSpot)
+        {
+            startTime = Time.time;
+            Transform oldPos = endPos;
+            endPos = startPos;
+            startPos = oldPos;
+            onSpot = false;
+        }
+        yield return new WaitForSeconds(3);
+        var truckManager = GameObject.Find("TruckManager").GetComponent<TruckSpawner>();
+        if (this.task == PortType.Import)
+        {
+            truckManager.importSpot = false;
+        }
+        if (this.task == PortType.Export)
+        {
+            truckManager.exportSpot = false;
+        }
+        truckManager.StartCoroutine(truckManager.NewTruck());
+        Destroy(gameObject);
+    }
 }
--- VR warehouse/Assets/Scripts/TruckSpawner.cs
+++ VR warehouse/Assets/Scripts/TruckSpawner.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TruckSpawner : MonoBehaviour
 {
+    public int ordersCompleted;
     public GameObject truck;
     public bool exportSpot;
     public bool importSpot;
@@ -22,16 +23,35 @@ public class TruckSpawner : MonoBehaviour
         {
            
             GameObject spawnedTruck = Instantiate(truck, importTrans.position, Quaternion.identity);
+            spawnedTruck.GetComponent<TruckController>().task = TruckController.PortType.Import;
             spawnedTruck.GetComponent<TruckController>().startPos = importSpawn;
             spawnedTruck.GetComponent<TruckController>().endPos = importTrans;
             importSpot = true;
+
         }
         if(!exportSpot)
         {
             GameObject spawnedTruck = Instantiate(truck, exportTrans.position, Quaternion.identity);
+            spawnedTruck.GetComponent<TruckController>().task = TruckController.PortType.Export;
             spawnedTruck.GetComponent<TruckController>().startPos = exportSpawn;
             spawnedTruck.GetComponent<TruckController>().endPos = exportTrans;
             exportSpot = true;
         }
     }
+    public IEnumerator NewTruck()
+    {
+        yield return new WaitForSeconds(5);
+        ordersCompleted++;
+        //if (ordersCompleted >= difficulty)
+        //{
+        //    //complete game
+        //}
+        //else
+        //{
+        //    this.GetComponent<TruckSpawner>().SpawnTruck();
+        //}
+       
+        StopCoroutine(NewTruck());
+    }
+    
 }
