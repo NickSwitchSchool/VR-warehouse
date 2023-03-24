daf196fe99aa19ebce98c6590176b696aa263701
NickSwitchSchool <97087180@st.deltion.nl>
2023-03-24 11:21:25 +0100

    button is always above product

--- VR warehouse/Assets/Scripts/Interactable.cs
+++ VR warehouse/Assets/Scripts/Interactable.cs
@@ -7,6 +7,12 @@ public class Interactable : MonoBehaviour
     [SerializeField] GameObject buttonDisplay;
     [SerializeField] float visibilityTime;
     float visibilityTimer;
+    float buttonHoverDistance;
+
+    private void Start()
+    {
+        buttonHoverDistance = transform.position.y - buttonDisplay.transform.position.y;    
+    }
 
     private void Update()
     {
@@ -16,6 +22,7 @@ public class Interactable : MonoBehaviour
             buttonDisplay.SetActive(true);
             buttonDisplay.transform.LookAt(GameObject.FindGameObjectWithTag("Player").transform.position + new Vector3(0, .5f, 0));
             buttonDisplay.transform.Rotate(90, 0, 0);
+            buttonDisplay.transform.position = transform.position - new Vector3(0, buttonHoverDistance, 0);
         }
         else
         {
