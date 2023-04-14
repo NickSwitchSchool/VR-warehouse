036056f5513d8d0be7b19b5cac2eb5cf47de8c99
Xander Veninga <39034729+EliteForce1@users.noreply.github.com>
2023-04-14 14:55:17 +0200

    Update ScoreCalc.cs

--- VR warehouse/Assets/ScoreCalc.cs
+++ VR warehouse/Assets/ScoreCalc.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreCalc : MonoBehaviour
 {
-    int totalDistance = 0;
+    float totalDistance = 0;
     int truckScore = 0;
+    public TextMeshProUGUI scoreText;
     // Start is called before the first frame update
     void Start()
     {
         totalDistance = PlayerPrefs.GetInt("Robot Distance");
         truckScore = PlayerPrefs.GetInt("TruckScore");
+
+        ChangeScore(totalDistance, truckScore);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ChangeScore(float _totalDisance, int _truckScore)
+    {
+        scoreText.text = _totalDisance.ToString();
     }
 }
